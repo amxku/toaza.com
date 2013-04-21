8efbcb2a240e477374e091947b58c1ca578a7ff5
amxku <amxku@sebug.net>
2013-04-21 21:22:14 +0800

    toaza 1.0

--- ginoa/model/_db.py
+++ ginoa/model/_db.py
@@ -7,19 +7,19 @@
 import ginoa.extensions.thing as thing
 
 db_config = {
-    'master' :{
-        'url':'mysql://ginoa:ginoa@127.0.0.1:3306/ginoa?charset=utf8',
-        'echo':False, #True,False
-        'pool_recycle':3600, #1小时链接一次
-        'pool_size':100, #连接数大小
-        'max_overflow':10, #超出pool_size后可允许的最大连接数
+    'master': {
+        'url': 'mysql://ginoa:ginoa@127.0.0.1:3306/ginoa?charset=utf8',
+        'echo': False, #True,False
+        'pool_recycle': 3600, #1小时链接一次
+        'pool_size': 100, #连接数大小
+        'max_overflow': 10, #超出pool_size后可允许的最大连接数
         },
-    'slave' :{
-        'url':'mysql://ginoa:ginoa@127.0.0.1:3306/ginoa?charset=utf8',
-        'echo':False,
-        'pool_recycle':3600,
-        'pool_size':100, 
-        'max_overflow':10,
+    'slave': {
+        'url': 'mysql://ginoa:ginoa@127.0.0.1:3306/ginoa?charset=utf8',
+        'echo': False,
+        'pool_recycle': 3600,
+        'pool_size': 100,
+        'max_overflow': 10,
         },
     }
 
